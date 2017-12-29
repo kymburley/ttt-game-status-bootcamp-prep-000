8b58729332d99acf9bb86dfc7cf97500d7038914
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-29 12:03:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,59 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], # top_row_win
+  [3, 4, 5], # middle_row_win
+  [6, 7, 8], # bottom_row_win
+  [0, 3, 6], # first_column_win
+  [1, 4, 7], # middle_column_win
+  [2, 5, 8], # last_column_win
+  [0, 4, 8], # first_diagonal_win
+  [2, 4, 6]  # second_diagonal_win
+]
+
+def won?(board)
+  # Is board empty?
+  counter = 0
+
+  board.each do |i|
+    if i == " "
+      counter += 1
+    end
+  end
+
+  if counter == 9
+    return false
+  end
+
+  #Do we have a winning combination?
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+       (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+    end
+  end
+end
+
+def full?(board)
+  if board.include?(" ")
+    return false
+  else
+    return true
+  end
+end
+
+def draw?(board)
+
+end
+
+def over?(board)
+
+end
